59df7a7cc360fc974c80921e5a5844cae908cd20
Juliana Adema <54901614+j-adema@users.noreply.github.com>
2019-09-18 11:52:27 -0400

    fixed error in coder1, created an error for coder 2

--- PS1.py
+++ PS1.py
@@ -13,11 +13,12 @@ Problem Set 1
 # first group member (coder 1), your job is to first correct it 
 # and make a new variable with an error for the next group member to fix
 # after competign both steps, commit and push your changes to GitHub
-coder1 = 'hello world! python line " + 1 
+coder1 = 'hello world! python line' + str(1) 
 print(coder1)
 
 # second group member's error to fix
-coder2 =
+coder2 = 42. + 13
+print(coder2)
 
 # now the second group member should define a variable with an error
 # and then commit and push changes to GitHub
